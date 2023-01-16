d96fac40404a230044476764c36450cfd7e76d32
Brennan Colberg <brennan.colberg@gmail.com>
2023-01-15 20:10:20 -0800

    actually create game when start button is pressed

--- src/index.ts
+++ src/index.ts
@@ -11,27 +11,26 @@ let game: GameState | undefined = undefined
 
 const button = document.querySelector("#menu button") as HTMLButtonElement
 button.addEventListener("click", start)
-export function start() {
-  const body = document.querySelector("body")
-  body?.classList.add("started")
-}
-
-const handler = (indexAfterLocation: number) => {
-  if (!game) return
-  try {
-    game = attemptToPlaceCard(game, indexAfterLocation)
-    render(game, handler)
-  } catch (e: any) {
-    alert(e.message)
-  }
-}
-
-async function resetGame() {
+async function start() {
+  // select + compose deck
   const decks = await Promise.all(deckUrls.map(loadEvents))
   const deck = decks.flat()
   console.log("starting game with deck", deck)
+
+  // start game
+  const handler = (indexAfterLocation: number) => {
+    if (!game) return
+    try {
+      game = attemptToPlaceCard(game, indexAfterLocation)
+      render(game, handler)
+    } catch (e: any) {
+      alert(e.message)
+    }
+  }
   game = createGame(deck)
   render(game, handler)
-}
 
-resetGame()
+  // mark as started (hides menu, shows game)
+  const body = document.querySelector("body")
+  body?.classList.add("started")
+}
