942405f5280cbf405ec4892abc989843ea89f463
Brennan Colberg <brennan.colberg@gmail.com>
2023-01-22 22:45:39 -0800

    compute, show score

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,16 @@ import { loadEvents, parseEventsFile } from "./io.js"
 
 let game: GameState | undefined = undefined
 
-const menu = document.querySelector("#menu") as HTMLFormElement
-menu.addEventListener("submit", start)
-async function start(event: SubmitEvent) {
+document.getElementById("menu")!.addEventListener("submit", start)
+async function start(event: SubmitEvent | MouseEvent) {
   event.preventDefault()
 
   // get selected decks
   const formData = new FormData(event.target as HTMLFormElement)
-  const deckURLs = formData.getAll('select-decks') as string[]
+  const deckURLs = formData.getAll("select-decks") as string[]
   const decks = await Promise.all(deckURLs.map(loadEvents))
 
-  const file = formData.get('upload-deck') as File
+  const file = formData.get("upload-deck") as File
   if (file?.size > 0) {
     const fileDeck = await parseEventsFile(file)
     decks.push(fileDeck)
@@ -35,11 +34,25 @@ async function start(event: SubmitEvent) {
       render(game, handler)
     } catch (e: any) {
       alert(e.message)
+      end()
     }
   }
   game = createGame(deck)
   render(game, handler)
 
   // mark as started (hides menu, shows game)
-  document.body.classList.add("started")
+  document.body.className = "game"
+}
+
+async function end() {
+  if (!game) throw new Error("cannot end game that does not exist")
+
+  // set fields
+  const correctCards = game.timeline.length - 1
+  document.getElementById("correct-cards")!.textContent = correctCards + ""
+  const score = Math.floor(Math.pow(correctCards, 1.1) * 100)
+  document.getElementById("score")!.textContent = score + ""
+
+  // show results
+  document.body.className = "results"
 }
